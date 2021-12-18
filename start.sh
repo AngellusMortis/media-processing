6a6a8346fef073352e51980ccdfa0232184f59ea
Christopher Bailey <cbailey@mort.is>
2021-12-17 19:21:09 -0500

    Tweak start

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+for f in $(find /etc/cron.d/ -type f); do
+    echo "Found crontab: $f"
+    chmod 644 "$f"
+    crontab "$f"
+done
+
 echo "Starting cron..."
-cron -f
+cron -f -l 15
